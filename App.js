bea657b97f32ec5c1a8d90ba8e2a9309ad539241
charlesworld1994@gmail.com <charlesryu@Charless-MacBook-Pro.local>
2019-06-20 19:15:29 -0700

    working on app.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component {
   render() {
     return (
       <View>
         <Header headerText={'ALBUMS'} />
+        <AlbumList />
       </View>
     );
   }
