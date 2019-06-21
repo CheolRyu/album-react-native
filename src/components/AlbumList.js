f08547d30b78b083fb46d0b4506b3605724232d4
charlesworld1994@gmail.com <charlesryu@Charless-MacBook-Pro.local>
2019-06-20 19:39:38 -0700

    worked on axios(fetch)

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import axios from 'axios';
 
 export default class AlbumList extends Component {
+  state = { albums: [] };
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => this.setState({ albums: res.data }));
+  }
   render() {
     const { textStyle, cardStyle } = styles;
+    console.log(this.state);
     return (
       <View style={cardStyle}>
         <Text style={textStyle}> ALBUM LIST </Text>
